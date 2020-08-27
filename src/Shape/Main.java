835c2b1da175e875d089743ea234528ca16720ff
pidhornyi86 <pidhornyi@ukr.net>
2020-08-27 21:06:04 +0300

    change

--- src/Shape/Main.java
+++ src/Shape/Main.java
@@ -2,9 +2,9 @@ package Shape;
 
 public class Main {
     public static void main(String[] args) {
-Shape Circle = new Circle();
-Shape Rectangle = new Rectangle();
-Circle.draw();
-Rectangle.draw();
+Shape circle = new Circle();
+Shape rectangle = new Rectangle();
+circle.draw();
+rectangle.draw();
     }
 }
